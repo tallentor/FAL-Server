be5329255437f2a7f45ae4d249d02358f43e32cd
chirath <chirathhasara2001@gmal.com>
2025-10-28 17:40:07 +0530

    create and update lawyers and calendars

---
+++ app/Http/Controllers/CalendarSlotController.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\CalendarSlot;
+use App\Http\Requests\StoreCalendarSlotRequest;
+use App\Http\Requests\UpdateCalendarSlotRequest;
+
+class CalendarSlotController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(StoreCalendarSlotRequest $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show(CalendarSlot $calendarSlot)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(UpdateCalendarSlotRequest $request, CalendarSlot $calendarSlot)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy(CalendarSlot $calendarSlot)
+    {
+        //
+    }
+}
